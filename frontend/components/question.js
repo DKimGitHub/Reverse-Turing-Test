3b43c38d36578f4c3164c4d2dc40fec42d364d40
Matthew Charters <matthew@charitycan.ca>
2023-12-21 14:37:28 -0500

    Use game test data to display game data

--- frontend/components/question.js
+++ frontend/components/question.js
@@ -1,9 +1,13 @@
 "use client";
+import Chat from "./chat";
 
-export default function Question() {
-    
-    return (
-
-
-    )
+const Question = ({ question }) => {
+  return (
+    <>
+      <Chat question={question.text} answer={question.player_answer} />
+      <Chat question={question.text} answer={question.bot_answer} />
+    </>
+  );
 }
+
+export default Question;
