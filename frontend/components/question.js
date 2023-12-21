01fb413bab9106d7d8f30f82304ad37595e8f24f
Daniel Kim <daniel@charitycan.ca>
2023-12-21 15:59:59 -0500

    fix bug for voting

--- frontend/components/question.js
+++ frontend/components/question.js
@@ -5,10 +5,10 @@ const Question = ({ question }) => {
   let humanFirst = localStorage.getItem('gameId')%2 ===0 ? true : false
   
   const voteHuman = () => {
-    fetch(`http://localhost:5000/api/answers/question.player_answer.id`)
+    fetch(`http://localhost:5000/api/answers/${question.player_answer.id}`)
   }
   const voteRobot = () => {
-    fetch(`http://localhost:5000/api/answers/question.bot_answer.id`)
+    fetch(`http://localhost:5000/api/answers/${question.bot_answer.id}`)
   }
   return (
     humanFirst ? 
