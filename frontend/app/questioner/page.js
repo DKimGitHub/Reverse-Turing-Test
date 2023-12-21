020250113949f542e3beaf64e1e8af0199687e43
Daniel Kim <daniel@charitycan.ca>
2023-12-21 14:51:16 -0500

    rearrange files

--- frontend/app/questioner/page.js
+++ frontend/app/questioner/page.js
@@ -1,15 +1,37 @@
 "use client";
 import AskQuestion from "../../components/askQuestion";
-import Chat from "../../components/chat";
-
+import { useState, useEffect } from "react";
+import Question from "../../components/question";
 export default function Questioner() {
-  return (
-    <div className="h-full">
-      <div className="h-4/5 grid grid-cols-2">
-        <Chat />
-        <Chat />
-      </div>
+  const [gameId, setGameId] = useState(null);
+  const [game, setGame] = useState(null);
 
+  useEffect(() => {
+    fetch("http://localhost:5000/api/games", { method: "POST" })
+      .then((res) => res.json())
+      .then((data) => {
+        setGameId(data.id);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!gameId) return;
+
+    fetch(`http://localhost:5000/api/games/${gameId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setGame(data);
+      });
+  }, [gameId]);
+
+  return (
+    <div>
+      <div className="grid grid-cols-2 bg-slate-500 rounded-md">
+        {game &&
+          game.questions.map((question) => (
+            <Question key={question.id} question={question} />
+          ))}
+      </div>
       <AskQuestion />
     </div>
   );
